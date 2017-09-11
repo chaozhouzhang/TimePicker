1fb657c52f53bd9ae233f12fa86eb094bb1c9ad1
chaozhouzhang <chaozhouzhang@qq.com>
2017-09-11 10:30:20 +0800

    Add Time Interval.

--- TimePickerDemo/app/src/main/java/me/zhangchaozhou/timepickerdemo/MainActivity.java
+++ TimePickerDemo/app/src/main/java/me/zhangchaozhou/timepickerdemo/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.text.ParseException;
+import java.util.Date;
+
+import me.zhangchaozhou.timepicker.util.DateUtil;
 import me.zhangchaozhou.timepicker.view.TimePicker;
 
 public class MainActivity extends AppCompatActivity implements TimePicker.OnSelectedListener {
@@ -22,5 +26,16 @@ public class MainActivity extends AppCompatActivity implements TimePicker.OnSele
     @Override
     public void onSelected(String time) {
         Log.i("time", time);
+        try {
+            Date date = DateUtil.parse(time, TimePicker.FORMAT_YMD_HMS);
+            Date now = new Date();
+            if (date.after(now)) {
+                Log.i("time", DateUtil.getDateInterval(date, now));
+            } else {
+                Log.i("time", DateUtil.getDateInterval(now, date));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- TimePickerDemo/timepicker/src/main/java/me/zhangchaozhou/timepicker/util/DateUtil.java
+++ TimePickerDemo/timepicker/src/main/java/me/zhangchaozhou/timepicker/util/DateUtil.java
@@ -6,9 +6,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Created by liuli on 2015/11/27.
- */
 public class DateUtil {
 
 
@@ -46,4 +43,33 @@ public class DateUtil {
         }
         return (returnValue);
     }
+
+    /**
+     * 计算时间间隔
+     *
+     * @param endDate
+     * @param startDate
+     * @return
+     */
+    public static String getDateInterval(Date endDate, Date startDate) {
+
+        long nd = 1000 * 24 * 60 * 60;
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+        long ns = 1000;
+        // 获得两个时间的毫秒时间差异
+        long diff = endDate.getTime() - startDate.getTime();
+        // 计算差多少天
+        long day = diff / nd;
+        // 计算差多少小时
+        long hour = diff % nd / nh;
+        // 计算差多少分钟
+        long min = diff % nd % nh / nm;
+        // 计算差多少秒
+        long sec = diff % nd % nh % nm / ns;
+        return day + "天" + hour + "小时" + min + "分钟" + sec + "秒";
+    }
+
+
+
 }
